30e1a02f233b4896f71aadb0520b2b400d979926
Joaquín P. Centeno <jpcenteno@users.noreply.github.com>
2020-07-26 23:16:33 -0300

    Set name & email

--- config.el
+++ config.el
@@ -6,8 +6,8 @@
 
 ;; Some functionality uses this to identify you, e.g. GPG configuration, email
 ;; clients, file templates and snippets.
-(setq user-full-name "John Doe"
-      user-mail-address "john@doe.com")
+(setq user-full-name "Joaquín P. Centeno"
+      user-mail-address "jpcenteno@users.noreply.github.com")
 
 ;; Doom exposes five (optional) variables for controlling fonts in Doom. Here
 ;; are the three important ones:
