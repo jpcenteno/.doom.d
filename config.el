a77a2f06b764783d647fe50f60d68826f4fcf989
Joaquín P. Centeno <jpcenteno@users.noreply.github.com>
2020-08-07 10:00:08 -0300

    Set fonts to Source [code|serif] Pro.

--- config.el
+++ config.el
@@ -20,10 +20,14 @@
 ;; + `doom-big-font' -- used for `doom-big-font-mode'; use this for
 ;;   presentations or streaming.
 ;;
+;; Reload the font settings with `(doom/reload-font)'
+;;
 ;; They all accept either a font-spec, font string ("Input Mono-12"), or xlfd
 ;; font string. You generally only need these two:
-;; (setq doom-font (font-spec :family "monospace" :size 12 :weight 'semi-light)
-;;       doom-variable-pitch-font (font-spec :family "sans" :size 13))
+(setq doom-font
+      (font-spec :family "Source Code Pro" :size 12 :weight 'medium)
+      doom-variable-pitch-font
+      (font-spec :family "Source Serif Pro" :size 12 :weight 'light))
 
 ;; There are two ways to load a theme. Both assume the theme is installed and
 ;; available. You can either set `doom-theme' or manually load a theme with the
