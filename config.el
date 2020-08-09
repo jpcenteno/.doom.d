a9945edb0bc582dda1e3e76c7061b9ac1af05bda
Joaquín P. Centeno <jpcenteno@users.noreply.github.com>
2020-08-09 00:22:16 -0300

    Make font a little bigger

--- config.el
+++ config.el
@@ -25,7 +25,7 @@
 ;; They all accept either a font-spec, font string ("Input Mono-12"), or xlfd
 ;; font string. You generally only need these two:
 (setq doom-font
-      (font-spec :family "Source Code Pro" :size 12 :weight 'medium)
+      (font-spec :family "Source Code Pro" :size 13 :weight 'medium)
       doom-variable-pitch-font
       (font-spec :family "Source Serif Pro" :size 12 :weight 'light))
 
