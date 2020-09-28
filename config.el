806da05ab4fc8c6d51333fcd161aa2230bdd74f4
Joaquín P. Centeno <jpcenteno@users.noreply.github.com>
2020-09-28 17:39:31 -0300

    Set default dir to ~

--- config.el
+++ config.el
@@ -1,8 +1,6 @@
 ;;; $DOOMDIR/config.el -*- lexical-binding: t; -*-
 
-;; Place your private configuration here! Remember, you do not need to run 'doom
-;; sync' after modifying this file!
-
+(setq default-directory "~/")
 
 ;; Some functionality uses this to identify you, e.g. GPG configuration, email
 ;; clients, file templates and snippets.
