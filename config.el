afe8b32b271076752a4d1905ddb5d47b46d976b1
Joaquín P. Centeno <jpcenteno@users.noreply.github.com>
2021-07-14 10:59:51 -0300

    Fix org agenda and capture templates config

--- config.el
+++ config.el
@@ -78,20 +78,21 @@
 ;; change `org-directory'. It must be set before org loads!
 (setq org-directory "~/org/")
 
-(setq org-agenda-files '("~/gtd/inbox.org"
-                         "~/gtd/gtd.org"
-                         "~/gtd/tickler.org"))
+(after! org
+  (setq org-agenda-files '("~/gtd/inbox.org"
+                           "~/gtd/gtd.org"
+                           "~/gtd/tickler.org"))
 
-(setq org-capture-templates '(("i" "Inbox entry" entry
-                               (file+headline "~/gtd/inbox.org" "Inbox")
-                               "* TODO %i%?")
-                              ("T" "Tickler" entry
-                               (file+headline "~/gtd/tickler.org" "Tickler")
-                               "* %i%? \n %U")))
+  (setq org-capture-templates '(("i" "Inbox entry" entry
+                                 (file+headline "~/gtd/inbox.org" "Inbox")
+                                 "* TODO %i%?")
+                                ("T" "Tickler" entry
+                                 (file+headline "~/gtd/tickler.org" "Tickler")
+                                 "* %i%? \n %U")))
 
-(setq org-refile-targets '(("~/gtd/gtd.org" :maxlevel . 3)
-                           ("~/gtd/someday.org" :level . 1)
-                           ("~/gtd/tickler.org" :maxlevel . 2)))
+  (setq org-refile-targets '(("~/gtd/gtd.org" :maxlevel . 3)
+                             ("~/gtd/someday.org" :level . 1)
+                             ("~/gtd/tickler.org" :maxlevel . 2))))
 
 (sp-with-modes 'org-mode
   ;; Code
